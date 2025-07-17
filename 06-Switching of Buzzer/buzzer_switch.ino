3a7a7145e9c5750ec21cfec3ad6ebcb912c16989
Mercy <76924137+ngozii@users.noreply.github.com>
2025-07-17 13:34:07 +0100

    Update buzzer_switch.ino

--- 06-Switching of Buzzer/buzzer_switch.ino
+++ 06-Switching of Buzzer/buzzer_switch.ino
@@ -1,1 +1,16 @@
+void setup() {
+  // Configure pin 2 as output to control BC548 transistor base
+  pinMode(2, OUTPUT);
+}
 
+void loop() {
+  // Turn buzzer ON by sending HIGH signal to transistor base
+  digitalWrite(2, HIGH);
+  delay(1000);              // Buzzer sounds for 1 second
+  
+  // Turn buzzer OFF by sending LOW signal to transistor base  
+  digitalWrite(2, LOW);
+  delay(1000);              // Buzzer silent for 1 second
+  
+  // Pattern repeats: 1 second ON, 1 second OFF
+}
